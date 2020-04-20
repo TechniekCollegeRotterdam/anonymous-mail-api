cd959de98821da8366c512a0a60debf41fedf30d
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-04-20 12:40:42 +0200

    get user data & send mail routes added

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,24 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
+const protectedRoute = require('./util/protectedRoute')
+
 const {
     signUpWithEmailAndPassword,
-    loginWithEmailAndPassword
+    loginWithEmailAndPassword,
+    getOwnUserData
 } = require('./handlers/users');
 
 const {
     sendMail
-} = require('./gmail')
+} = require('./handlers/gmail')
 
 // User
 app.post('/signUpWithEmailAndPassword', signUpWithEmailAndPassword);
 app.post('/loginWithEmailAndPassword', loginWithEmailAndPassword)
+app.get('/getOwnUserData/:username', protectedRoute, getOwnUserData)
 
 // Mail
-app.get('/sendMail', sendMail)
+app.post('/sendMail', protectedRoute, sendMail)
 
 exports.api = functions.region('europe-west2').https.onRequest(app)
