35a5f7aadc5c52b1abc4e69ff7578d32443478d4
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-04-20 12:39:53 +0200

    route guard added

---
+++ functions/util/protectedRoute.js
@@ -0,0 +1,28 @@
+const { admin, db } = require('./admin');
+
+module.exports = async (req, res, next) => {
+
+    try {
+        let idToken;
+        if (req.headers.authorization) {
+            idToken = req.headers.authorization
+        } else {
+            console.error('No token found');
+            return res.status(403).json({ error: 'Unauthorized' });
+        }
+
+        // Get decoded token
+        req.user = await admin.auth().verifyIdToken(idToken)
+
+        const data = await db.collection('users').where('userId', '==', req.user.uid).limit(1).get()
+
+        req.user.username = data.docs[0].data().username
+
+        return next()
+
+    } catch (err) {
+        console.error('Error while verifying token', err);
+        return res.status(403).json(err);
+    }
+
+}
