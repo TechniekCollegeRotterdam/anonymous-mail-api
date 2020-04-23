0361f72228f78ecda8fc8cccb2d7c8af2454b29f
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-04-23 13:11:12 +0200

    Get, add & delete routes added

--- functions/index.js
+++ functions/index.js
@@ -13,6 +13,12 @@ const {
     sendMail
 } = require('./handlers/gmail')
 
+const {
+    addSpamEmailAddress,
+    deleteSpamEmailAddress,
+    getSpamEmailAddresses
+} = require('./handlers/database')
+
 // User
 app.post('/signUpWithEmailAndPassword', signUpWithEmailAndPassword);
 app.post('/loginWithEmailAndPassword', loginWithEmailAndPassword)
@@ -20,5 +26,11 @@ app.get('/getOwnUserData/:username', protectedRoute, getOwnUserData)
 
 // Mail
 app.post('/sendMail', protectedRoute, sendMail)
+// TODO: Place given spam email adresses in gmail spam folder
+
+// Database
+app.post('/addSpamEmailAddress', protectedRoute, addSpamEmailAddress)
+app.delete('/deleteSpamEmailAddress/:emailId', protectedRoute, deleteSpamEmailAddress)
+app.get('/getSpamEmailAddresses', protectedRoute, getSpamEmailAddresses)
 
 exports.api = functions.region('europe-west2').https.onRequest(app)
