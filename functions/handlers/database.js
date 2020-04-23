84585fc2c1d35bc33165de662a290fa7452c8080
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-04-23 13:09:08 +0200

    Delete spam email route added

--- functions/handlers/database.js
+++ functions/handlers/database.js
@@ -28,3 +28,30 @@ exports.addSpamEmailAddress = async (req, res) => {
 }
 
 
+exports.deleteSpamEmailAddress = async (req, res) => {
+
+    const document = db.doc(`/spammedEmails/${req.params.emailId}`)
+    document.get()
+        .then(doc => {
+            if (!doc.exists) {
+                return res.status(404).json({error: 'Email address not found'})
+            }
+            if (doc.data().userHandle !== req.user.handle) {
+                return res.status(403).json({error: 'Unauthorized'})
+            } else {
+                return document.delete()
+            }
+        })
+        // eslint-disable-next-line promise/always-return
+        .then(() => {
+            res.json({message: 'Spammed email address deleted successfully'})
+        })
+        .catch(err => {
+            console.error(err)
+            if (err.code === "auth/id-token-expired")
+                return res.status(401).json({general: 'Login expired. Please login again'})
+            else
+                return res.status(500).json({error: err.code})
+        })
+}
+
